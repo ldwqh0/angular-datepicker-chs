70583f8cc6bfa79839670f04b7025079b8c08738
xz <ldwqh0@outlook.com>
2017-08-24 18:33:15 +0800

    更改内核

--- src/datepicker.js
+++ src/datepicker.js
@@ -3,225 +3,88 @@ import template from './datepicker.html'
 import moment from 'moment'
 import angular from 'angular'
 
-const DAYS_IN_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-let DAYS = ['日', '一', '二', '三', '四', '五', '六']
-const VIEWS = {
-  year: 0,
-  month: 1,
-  day: 2,
-  hour: 3
-}
-
-/**
- * 判断视图a是不是在视图b之后
- * @param a
- * @param b
- */
-function viewAfter(a, b) {
-  return VIEWS[a] - VIEWS[b] > 0
-}
-
-/**
- * 判断一个元素是不是另外一个元素的子代
- * @param source
- * @param target
- */
-function isChildrenOf(source, target) {
-  if (target.parentNode) {
-    let parent = target.parentNode
-    if (parent === source) {
-      return true
-    } else {
-      return isChildrenOf(source, parent)
-    }
-  } else {
-    return false
-  }
-}
-
-/**
- * 获取指定年月的天数
- * @param year
- * @param month
- * @returns {number}
- */
-function getDays(year, month) {
-  if (month === 1) {
-    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
-      return 29
-    }
-  }
-  return DAYS_IN_MONTH[month]
-}
-
-function getValue(type, date) {
-  if (date) {
-    let v = date
-    if (viewAfter('hour', type)) {
-      v.millisecond(0)
-      v.second(0)
-      v.minute(0)
-      v.hour(0)
-    }
-
-    if (viewAfter('day', type)) {
-      v.day(1)
-    }
-    if (viewAfter('month', type)) {
-      v.month(0)
-    }
-    return v
-  }
-}
-
 let module = angular.module('datepicker', ['ui.bootstrap'])
 
-
 class DatePickerController {
-  static $inject = ['$element']
-  current = moment()
-  view = 'day'
-  years = []
-  months = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-  days = []
-  minView = 'day'
+  static $inject = ['$scope']
+
+  constructor($scope) {
+    this.$scope = $scope
+  }
+
+  // datepicekr选项
+  datepickerOptions = {
+    showWeeks: false,
+    yearColumns: 3,
+    yearRows: 4
+  }
+  // 弹框状态
+  isOpen = false
+  // 格式化样式
   format = 'yyyy-MM-dd'
-  value = this.current.toDate()
 
-  constructor($element) {
-    this.$element = $element
-  }
-
-  $onChanges(obj) {
-    if (obj.minView) {
-      if (this.value) {
-        this.value = getValue(this.minView, moment(this.value)).toDate()
-      }
-      if (viewAfter(this.view, this.minView)) {
-        this.view = this.minView
-      }
+  $doCheck() {
+    // 将日期什么的小时，分钟数等等置为空
+    if (this.ngModel && this.ngModel instanceof Date) {
+      this.ngModel.setSeconds(0)
+      this.ngModel.setHours(0)
+      this.ngModel.setMinutes(0)
+      this.ngModel.setMilliseconds(0)
     }
   }
 
-  next() {
-    switch (this.view) {
-      case 'year':
-        this.current.add(12, 'y')
-        break
-      case 'month':
-        this.current.add(1, 'y')
-        break
-      case 'day':
-        this.current.add(1, 'M')
-        break
-    }
-    this.setCurrent()
-  }
-
-  prev() {
-    switch (this.view) {
-      case 'year':
-        this.current.subtract(12, 'y')
-        break
-      case 'month':
-        this.current.subtract(1, 'y')
-        break
-      case 'day':
-        this.current.subtract(1, 'M')
-        break
-    }
-    this.setCurrent()
-  }
-
-  setView(type) {
-    if (viewAfter(type, this.minView)) {
-      this.value = getValue(this.minView, this.current).toDate()
-      this.isOpen = false
-      this.ngChange()
+  open() {
+    if (this.disabled === '' || this.disabled === 'disabled' || this.disabled === true || this.disabled === 'true') {
     } else {
-      this.view = type
+      this.isOpen = !this.isOpen
     }
   }
 
-  isCurrent(type, value) {
-    switch (type) {
-      case "year":
-        return this.value.getFullYear() === value
-        break
-      case "month":
-        return this.value.getMonth() === value
-        break
-      case "day":
-        return this.value.getDay() === value
-        break
+  $onChanges(changes) {
+    if (changes.options) {
+      this.datepickerOptions = Object.assign(this.datepickerOptions, this.options)
     }
-  }
-
-  setCurrent(data) {
-    let $ctrl = this
-    if (data) {
-      if (data.year) {
-        this.current.year(data.year)
+    if (changes.minView) {
+      this.datepickerOptions.minMode = this.minView
+      if (this.minView === 'year' && this.ngModel && this.ngModel instanceof Date) {
+        this.ngModel.setMonth(0).setDate(1)
       }
-      if (data.month) {
-        this.current.month(data.month)
-      }
-      if (data.date) {
-        this.current.date(data.date)
+      if (this.minView === 'month' && this.ngModel && this.ngModel instanceof Date) {
+        this.ngModel.setDate(1)
       }
     }
-
-    // 在当前的视图值改变之后，设置页面列表项
-    let year = this.current.year()
-    let month = this.current.month()
-    let date = this.current.date()
-    let years = []
-    let days = []
-    for (let i = year - 5; i < year + 7; i++) {
-      years.push(i)
+    if (changes.maxView) {
+      this.datepickerOptions.maxMode = this.maxView
     }
-    this.years = years
-    let dayCount = getDays(year, month)
-    for (let i = 0; i < dayCount; i++) {
-      days.push({year, month, date: i + 1})
+    if (changes.minDate) {
+      this.datepickerOptions.minDate = this.minDate
+      if (!moment(this.ngMdodel).isAfter(this.minDate)) {
+        this.ngModel = this.minDate
+      }
     }
-
-    let first = moment(new Date(year, month, days[0].date))
-    let last = moment(new Date(year, month, dayCount))
-    for (let i = first.day(); i > 0; i--) {
-      first.subtract(1, 'd')
-      days.unshift({year: first.year(), month: first.month(), date: first.date()})
+    if (changes.maxDate) {
+      this.datepickerOptions.maxDate = this.maxDate
+      if (!moment(this.ngModel).isBefore(this.maxDate)) {
+        this.ngModel = this.maxDate
+      }
     }
-    for (let i = last.day(); i < 6; i++) {
-      last.add(1, 'd')
-      days.push({year: last.year(), month: last.month(), date: last.date()})
-    }
-
-    $ctrl.days = days
-    return true
   }
-
-  getDisabled() {
-    return this.$element.attr('disabled') === 'disabled'
-  }
-
-  $onInit() {
-    this.setCurrent()
-  }
-
 }
 
 let component = {
   template,
   controller: DatePickerController,
   bindings: {
-    value: "=?ngModel",
-    minView: "@?",
+    ngModel: '=',
+    options: '<?',
+    minView: '@?',
+    maxView: '@?',
     format: '@?',
-    ngChange: '&'
+    maxDate: '<?',
+    minDate: '<?',
+    disabled: '@?'
   }
 }
-
 module.component('datepicker', component)
 
 export default 'datepicker'
